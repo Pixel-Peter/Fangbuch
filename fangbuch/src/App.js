ae0b1b7b03ef2ada1b114746c76c82ec48d40be9
Peter "Pixel" Hartauer <78911783+Pixel-Peter@users.noreply.github.com>
2023-03-22 14:09:32 +0100

    Update App.js

--- fangbuch/src/App.js
+++ fangbuch/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { render } from 'react-dom';
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <button className="menu-icon" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </button>
+      <nav className={`menu ${menuOpen ? 'open' : ''}`}>
+        <ul>
+          <li>Menu Item 1</li>
+          <li>Menu Item 2</li>
+          <li>Menu Item 3</li>
+        </ul>
+      </nav>
+      <main>
+        <h1>Progressive Web App</h1>
+        <div className="text-block">This is an informative text</div>
+        <div className="pie-chart-area"></div>
+      </main>
     </div>
   );
 }
+
+render(<App />, document.getElementById('root'));
 
 export default App;
